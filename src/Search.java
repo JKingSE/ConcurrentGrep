ec10ff1f062d40d33de7b676a3b49bad1aa92c27
Norwal <jfk1307@g.rit.edu>
2016-10-28 10:12:44 -0400

    Changed some stuff

--- src/Search.java
+++ src/Search.java
@@ -10,11 +10,8 @@ import java.util.List;
 import java.io.FileInputStream;
 import java.util.concurrent.Callable;
 
-/**
- * Search searches the input stream for a given pattern
- *
- * Created by John King on 27-Oct-16.
- */
+/**Search searches the input stream for a given pattern
+ * Created by John King on 27-Oct-16. */
 public class Search implements Callable<ListOfFound> {
     // The Found object to store search results
     private final ListOfFound results;
@@ -23,68 +20,53 @@ public class Search implements Callable<ListOfFound> {
     // The pattern
     private final String pattern;
 
-    /**
-     * Constructor for file
-     *
+    /** Constructor for file
      * @param file
      * @param pattern
-     * @throws FileNotFoundException
-     */
-    public Search(File file, String pattern)
-            throws FileNotFoundException {
+     * @throws FileNotFoundException */
+    public Search(File file, String pattern) throws FileNotFoundException {
         this.stream = new FileInputStream(file);
         this.results = new ListOfFound();
         this.results.setName(file.getName());
         this.pattern = pattern;
     }
 
-    /**
-     * Constructor for stdin
-     *
-     * @param input
-     * @param pattern
-     */
-    public Search(InputStream input, String pattern) {
-        this.stream = input;
-        this.results = new ListOfFound();
-        this.pattern = pattern;
-        this.results.setName("-");
-    }
-
-    /**
-     * Search for the pattern in the input stream
-     *
-     * @return results - Found object with a list of the lines that have the
-     *         pattern
-     * @throw Exception - If no results are found
-     */
+    /** Search for the pattern in the input stream
+     * @return results - Found object with a list of the lines that have the pattern
+     * @throw Exception - If no results are found */
     @Override
     public ListOfFound call() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                stream));
+        Pattern pattern = Pattern.compile(this.pattern);
+
         // Matching lines
-        List<String> list = new ArrayList<String>();
+        List<String> strings = new ArrayList<String>();
 
         // The current line
         String currentLine;
 
         // The current line number
-        long lineCount = 0;
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(
-                stream));
-        Pattern expression = Pattern.compile(pattern);
+        long line = 0;
 
         while ((currentLine = reader.readLine()) != null) {
-            Matcher matcher = expression.matcher(currentLine);
-
+            Matcher matcher = pattern.matcher(currentLine);
             if (matcher.find()) {
-                list.add(lineCount + " " + currentLine);
+                strings.add(line + " " + currentLine);
             }
-
-            lineCount++;
+            line++;
         }
-
-        results.setMatches(list);
-
+        results.setMatches(strings);
         return results;
+    }
+
+    /** Constructor for stdin
+     * @param input
+     * @param pattern */
+    public Search(InputStream input, String pattern) {
+        this.stream = input;
+        this.pattern = pattern;
+        this.results = new ListOfFound();
+        this.results.setName("-");
     }
 }
