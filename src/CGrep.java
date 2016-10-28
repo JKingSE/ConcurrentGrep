d648c17d9d255acb23f5df00706cd10291017a7b
Axiat <nate.ortega95@gmail.com>
2016-10-28 19:30:53 -0400

    - completed the CGrep class

--- src/CGrep.java
+++ src/CGrep.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -15,59 +16,74 @@ import java.util.concurrent.Future;
 public class CGrep {
 
     // Thread Pool Size
-    private static final int THREADPOOLAMOUNT = 3;
+    private static final int THREAD_POOL_MAX = 3;
+    private static  ExecutorService executor;
 
-    /**
-     * Main method used to configure the application
-     *
-     * @param args
-     *            pattern [file1] [file2] (optional)
-     */
     public static void main(String[] args) {
+        int argLength = args.length;
 
-        ExecutorService executor = Executors
-                .newFixedThreadPool(THREADPOOLAMOUNT);
+        if(argLength == 0){
+            System.err.println("Missing Search Pattern"); // exit program if no search patter is given
+            System.exit(1);
+        }
 
-        String pattern = args[0].toString();
+        executor = Executors.newFixedThreadPool(THREAD_POOL_MAX);
 
-        // Check lenght of arguments
-        if (args.length > 1) {
-            for (int i = 1; i < args.length; i++) {
-                String argument = args[i].toString();
-                File file = new File(argument);
+        String pattern = args[0];
 
-                // Search the file
-                if (file.exists()) {
-                    try {
-                        Callable<ListOfFound> searcher = new Search(file,
-                                pattern);
-                        Future<ListOfFound> result = executor.submit(searcher);
-                        System.out.println(result.get());
-                    } catch (FileNotFoundException e) {
-                        System.err.print(e.toString());
-                    } catch (InterruptedException e) {
-                        System.err.print(e.toString());
-                    } catch (ExecutionException e) {
-                        System.err.print(e.toString());
-                    }
-                }
-                else{
-                    System.out.println(argument + " not found");
-                }
-            }
-        } else if (args.length == 1) {
-            InputStream input = System.in;
-            Callable<ListOfFound> searcher = new Search(input, pattern);
-            Future<ListOfFound> result = executor.submit(searcher);
-            try {
-                System.out.println(result.get());
-            } catch (InterruptedException e) {
-                System.err.print(e.toString());
-            } catch (ExecutionException e) {
-                System.err.print(e.toString());
-            }
+
+        if (argLength > 1) { // case where all arguments are passed through program args
+            searchFiles(args,pattern,false);
+
+        } else if (argLength == 1) { // case where the user supplies their own filepath's via keyboard
+            System.out.print("Please enter filepath(s) separated by spaces: ");
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            String[] parsed = input.split(" "); // split on all spaces
+            searchFiles(parsed,pattern,true);
         }
 
         executor.shutdown();
+    }
+
+
+    /**
+     * Takes an array of filepaths and the pattern to search for, opens the files
+     * and performs the "Main" loop of execution for our program. if manualInput is
+     * true, then this tells method that the array of filepath's are coming from the
+     * keyboard vs from the program arguments when it's false.
+     * @param input
+     * @param pattern
+     * @param manualInput tells method where the filepath's String away is coming from.
+     */
+    public static void searchFiles(String[] input, String pattern, boolean manualInput){
+        int length = input.length;
+
+        int i = manualInput ? 0 : 1 ; // skips the first element in the array if taking input from program arguments
+        for(;i<length;i++){
+
+            String filepath = input[i];
+            File file = new File(filepath);
+
+            if(filepath.isEmpty()){ // ignore empty spaces and skip iteration
+                continue;
+            }
+            else if (file.exists()) {
+                try {
+
+                    Callable<ListOfFound> searcher = new Search(file, pattern);
+                    Future<ListOfFound> result = executor.submit(searcher);
+                    System.out.println(result.get());
+
+                } catch (FileNotFoundException | ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            else{
+                System.err.println("'" + filepath + "'" + " not found");
+            }
+
+        }
+
     }
 }
